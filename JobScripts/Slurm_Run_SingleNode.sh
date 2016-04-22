7b08f4deef942a5ab869f2bb4f7a72307bc40e8d
Felipe Gutierrez <fgutierrez3@wisc.edu>
2016-04-21 20:43:39 -0500

    change write sim params spot so they get the correct values

--- JobScripts/Slurm_Run_SingleNode.sh
+++ JobScripts/Slurm_Run_SingleNode.sh
@@ -3,12 +3,12 @@
 #SBATCH -N 1 -n 32 -t 0-20:00:00
 #SBATCH -o charm-run-log.o%j
 
-cd /home/felipegb94/repositories/CharmSPH
+cd /srv/home/felipegb94/repositories/CharmSPH
 make clean
 make
 
 
-./charmrun +p32 ./charmsph -x 2 -y 2 -z 2 -t 2 -h 0.01 -dt 0.0002 -wb 1 -csm 4 -wp 250 -lbp 200
-./charmrun +p16 ./charmsph -x 2 -y 2 -z 2 -t 2 -h 0.01 -dt 0.0002 -wb 1 -csm 4 -wp 250 -lbp 200
-./charmrun +p8 ./charmsph -x 2 -y 2 -z 2 -t 2 -h 0.01 -dt 0.0002 -wb 1 -csm 4 -wp 250 -lbp 200
+./charmrun +p32 ./charmsph -x 2 -y 2 -z 2 -t 20000 -h 0.02 -dt 0.0002 -wb 1 -csm 4 -wp 250 -lbp 200
+./charmrun +p16 ./charmsph -x 2 -y 2 -z 2 -t 20000 -h 0.02 -dt 0.0002 -wb 1 -csm 4 -wp 250 -lbp 200
+./charmrun +p8 ./charmsph -x 2 -y 2 -z 2 -t 20000 -h 0.02 -dt 0.0002 -wb 1 -csm 4 -wp 250 -lbp 200
 
